d1758fc269f15d1736c504ab3da1fe171cd46c6a
Alfonso Conejo <49042515+AlfonsoConejo@users.noreply.github.com>
2026-05-15 22:33:51 -0600

    Activate button when fields are correct

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,57 @@
 import { Link } from 'react-router-dom';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function LoginForm() {
   const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({ email: '', password: ''});
+  const [isSending, setIsSending] = useState(false);
+  const [isFormValid, setIsFormValid] = useState({email: false, password: false});
+  const [serverError, setServerError] = useState("");
+
+  //Validate empty fields
+  const validateForm = (name, value) => {
+    value = value.trim();
+
+    switch (name) {
+      case "email":
+      setIsFormValid(prev => ({
+        ...prev,
+        email: /\S+@\S+\.\S+/.test(value)
+      }));
+      break;
+
+      case "password":
+        setIsFormValid(prev => ({
+          ...prev,
+          password: value.length >= 6
+        }));
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData(prev => ({
+      ...prev,
+      [name]: value
+    }));
+
+    validateForm(name, value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+      document.title = "Inicio de sesión";
+    }, []);
+
+  console.log("setIsFormValid equals: " + JSON.stringify(isFormValid));
 
   return (
     <div className="min-h-screen bg-gray-900 flex items-center justify-center px-4">
@@ -27,32 +76,47 @@ export default function LoginForm() {
         <div className="text-center text-gray-400 text-sm mb-6">o</div>
 
         {/* FORM */}
-        <form className="space-y-4">
+        <form onSubmit={handleSubmit} className="space-y-4">
+          {serverError && (
+            <div className="bg-red-500/10 border border-red-500 text-red-400 p-2 rounded mb-4 text-sm">
+              {serverError}
+            </div>
+          )}
           
           {/* EMAIL */}
           <div>
-            <label className="block text-sm text-gray-400 mb-1">
+            <label htmlFor="email" className="block text-sm text-gray-400 mb-1">
               Correo
             </label>
             <input
+              onChange={handleChange}
+              id="email"
               type="email"
               name="email"
+              value={formData.email}
               placeholder="correo@email.com"
+              maxLength={254}
+              autoComplete="email"
               className="w-full bg-gray-700 border border-gray-600 rounded-md px-3 py-2 text-white placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-blue-500"
             />
           </div>
 
           {/* PASSWORD */}
           <div>
-            <label className="block text-sm text-gray-400 mb-1">
+            <label htmlFor="password" className="block text-sm text-gray-400 mb-1">
               Contraseña
             </label>
 
             <div className="relative">
               <input
+                onChange={handleChange}
+                id="password"
                 type={showPassword ? "text" : "password"}
                 name="password"
+                value={formData.password}
                 placeholder="••••••••"
+                maxLength={72}
+                autoComplete="password"
                 className="w-full bg-gray-700 border border-gray-600 rounded-md px-3 py-2 pr-10 text-white placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-blue-500"
               />
 
@@ -81,10 +145,13 @@ export default function LoginForm() {
 
           {/* SUBMIT */}
           <button
+            disabled={ !(isFormValid.email && isFormValid.password) }
             type="submit"
-            className="w-full bg-blue-600 hover:bg-blue-500 text-white py-2 rounded-md font-semibold transition"
-          >
-            Iniciar sesión
+            className="flex justify-center w-full bg-blue-600 text-white py-2 rounded-md font-semibold transition
+              hover:bg-blue-500 cursor-pointer
+              disabled:bg-blue-300 disabled:cursor-not-allowed disabled:opacity-50"
+            >
+            {isSending ? <div className="loader "></div> : 'Iniciar sesión' }
           </button>
         </form>
 
