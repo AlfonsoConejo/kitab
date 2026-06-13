95843d5392945903e1f4987b3c36cca9c59b7db3
Alfonso Conejo <49042515+AlfonsoConejo@users.noreply.github.com>
2026-06-12 18:09:19 -0600

    Added state to track periods
    -Consummed endpoint to get all the periods of the corresponding user.
    -Added conditional rendering when there are more than 0 periods.

--- src/pages/Period.jsx
+++ src/pages/Period.jsx
@@ -1,7 +1,41 @@
 import { Link } from "react-router-dom";
 import NoActivePeriodMessage from "@/components/NoActivePeriodMessage";
+import { apiFetch } from "@/services/apiFetch";
+import { useState, useEffect } from "react";
+import { notify } from "@/utils";
 
 export default function Period() {
+
+  const [periods, setPeriods] = useState([]);
+
+  useEffect(()=>{
+    async function fetchPeriods() {
+      try{
+        const res = await apiFetch("/api/periods", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          }
+        });
+
+        const data = await res.json();
+        if (!res.ok) {
+          notify("error", "Hubo un error en el servidor.");   
+          return;
+        }
+
+        setPeriods(data.data);
+      } catch (error){
+        if (error.message === "SESSION_EXPIRED") {
+          return;
+        }
+      }
+    }
+    fetchPeriods();
+  }, []);
+
+  console.log("Periodos del usuario: " + JSON.stringify(periods));
+
   return (
     <div className="flex flex-col flex-1 gap-6">
       <div className="flex justify-between items-center">
@@ -15,7 +49,14 @@ export default function Period() {
       </div>
 
       <div className="flex-1">
-        <NoActivePeriodMessage/>
+        {
+          periods.length === 0 ? (<NoActivePeriodMessage/>) : (
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+              HI
+            </div>
+          )
+        }
+        
       </div>
     </div>
   );
